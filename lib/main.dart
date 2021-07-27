b0a3c4d8ad50942f2ff1ed6667e0fc2f8dd480e2
dinesh parmar <shashikha1000@gmail.com>
2021-07-27 13:40:34 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapp/recycler/RecyclerClass.dart';
-import 'package:flutterapp/SplashScreen.dart';
+import 'package:flutter/rendering.dart';
+// import 'package:flutterapp/recycler/RecyclerClass.dart';
+// import 'package:flutterapp/SplashScreen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home:SplashScreen()
-    )
-  );
+  runApp(MyApp());
+
 }
+
+//
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MainApp",
+
+      home: Scaffold(
+
+        drawer: Drawer(
+
+        ),
+        appBar: AppBar(
+          title: Text("My App"),
+
+        ),
+    body: Center(
+    child: new Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    InkWell(
+    splashColor: Colors.green,
+    highlightColor: Colors.blue,
+    child: Icon(Icons.ring_volume, size: 50),
+    onTap: () {
+    // setState(() {
+    _volume += 10;
+    // });
+    },
+    ),
+ Text(
+    _volume.toString(),
+    style: TextStyle(fontSize: 50)
+    ),
+
+
+
+
+
+
+
+
+
+  ]
+      ),
+    ),
+
+    )
+    );
+
+  }
+
+}
+int _volume = 0;
+
 
 
 
