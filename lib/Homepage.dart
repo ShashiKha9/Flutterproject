9261d4437680881aaf0eaf4f43e47a3af0052440
dinesh parmar <shashikha1000@gmail.com>
2021-08-11 12:42:09 +0530

    adding catalog json

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,16 +1,31 @@
 
+import 'dart:convert';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterapp/models/catlog.dart';
 import 'package:flutterapp/widgets/drawer.dart';
 import 'package:flutterapp/widgets/item_widget.dart';
 
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
   @override
+  HomePageState createState() => HomePageState();
+  }
+  class HomePageState extends State<HomePage> {
+@override
+ void initState(){
+  super.initState();
+  loadData();
+}
+loadData()async {
+var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+var decodedData = jsonDecode(catalogJson);
+print(decodedData);
+}
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
